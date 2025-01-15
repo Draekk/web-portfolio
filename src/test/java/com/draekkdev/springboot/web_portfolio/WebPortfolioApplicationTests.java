05bc7896678f61ea933f3c38e7a12f00316ec899
Draekk <rodriver1992@gmail.com>
2025-01-15 19:50:05 -0300

    upload: initial files

---
+++ src/test/java/com/draekkdev/springboot/web_portfolio/WebPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draekkdev.springboot.web_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
