ce20a92b478160cf3f770782a2b66253bc9843b4
Draekk <rodriver1992@gmail.com>
2025-01-25 12:18:06 -0300

    upd: changed CORS config

---
+++ server/src/main/java/com/draekkdev/springboot/web_portfolio/WebConfig.java
@@ -0,0 +1,18 @@
+package com.draekkdev.springboot.web_portfolio;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(false);
+    }
+}
