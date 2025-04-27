5753c8a429740020928628f6ffeda33d114a6e67
draekkdev <rodriver1992@gmail.com>
2025-04-26 23:38:29 -0400

    updated pom.xml | fixed token interceptor's token comparision

--- server/src/main/java/com/draekkdev/springboot/web_portfolio/interceptors/TokenInterceptor.java
+++ server/src/main/java/com/draekkdev/springboot/web_portfolio/interceptors/TokenInterceptor.java
@@ -1,11 +1,12 @@
 package com.draekkdev.springboot.web_portfolio.interceptors;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class TokenInterceptor implements HandlerInterceptor {
@@ -26,7 +27,8 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     String token = request.getHeader("Authorization");
 
-    if (token == null || !token.equals(secretToken)) {
+    if (token == null || !token.equalsIgnoreCase(secretToken)) {
+      System.out.println(token + " " + secretToken);
       response.setStatus(HttpStatus.UNAUTHORIZED.value());
       response.getWriter().write("Unauthorized: Invalid Token.");
       return false;
