9bf3b2a5c9d275159508dc0e85121d9b4ea264c2
Draekk <rodriver1992@gmail.com>
2025-05-02 19:07:36 -0400

    modified screenshot viewer styles

--- client/src/components/technologies/projects/ScreenshotViewer.tsx
+++ client/src/components/technologies/projects/ScreenshotViewer.tsx
@@ -16,26 +16,30 @@ function ScreenshotViewer({
   return (
     <div
       id="screenshot-viewer"
-      className="absolute h-screen top-0 left-0 w-screen flex flex-col justify-center items-center"
+      className="absolute h-screen top-0 left-0 z-50 w-screen py-2 lg:py-4 flex flex-col justify-between items-center"
     >
       <div
         onClick={() => setScreenshotViewer(false)}
-        className="self-end border border-white rounded-full w-7 h-7 lg:w-10 lg:h-10 flex items-center justify-center mr-10 lg:mr-32 cursor-pointer"
+        className="self-end border border-white rounded-full w-7 h-7 lg:w-10 lg:h-10 flex items-center justify-center mt-5 mr-10 lg:mr-32 cursor-pointer"
       >
         <i className="fa-solid fa-xmark lg:text-3xl"></i>
       </div>
-      <figure className="max-h-[50%] max-w-[90%] my-5 rounded-lg overflow-hidden">
-        <img className="w-full h-full" src={picUrl} alt="project-screenshot" />
+      <figure className="flex items-center justify-center h-[70%] min-w-fit max-w-[90%] my-5 rounded-sm md:rounded-lg overflow-x-auto overflow-y-hidden">
+        <img
+          className="w-full h-full object-contain"
+          src={picUrl}
+          alt="project-screenshot"
+        />
       </figure>
       <ul className="flex gap-x-5 justify-center w-[90%]">
         {screenshots.map((s) => (
           <li key={s.id}>
-            <figure className="w-20 h-20 overflow-hidden rounded-lg flex items-center justify-center hover-pic">
+            <figure className="w-16 h-16 md:w-20 md:h-20 overflow-hidden rounded-sm md:rounded-lg flex items-center justify-center hover-pic target:border-2 target:border-teal-400">
               <img
                 onClick={(e) => setPicUrl(e.currentTarget.src)}
                 src={s.url}
                 alt="project-screenshot"
-                className="w-full cursor-pointer"
+                className="w-full h-full object-cover cursor-pointer"
               />
             </figure>
           </li>
