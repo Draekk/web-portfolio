52e342c55a456977eb1abe3f302c2a63b76cd03d
Draekk <rodriver1992@gmail.com>
2025-05-13 19:46:20 -0400

    updated screenshot viewer styles

--- client/src/components/technologies/projects/ScreenshotViewer.tsx
+++ client/src/components/technologies/projects/ScreenshotViewer.tsx
@@ -1,3 +1,4 @@
+import "../../../App.css";
 import { tScreenshot } from "../../../types/tProject";
 
 type tScreenshotViewerProps = {
@@ -16,25 +17,27 @@ function ScreenshotViewer({
   return (
     <div
       id="screenshot-viewer"
-      className="absolute h-screen top-0 left-0 z-50 w-screen py-2 lg:py-4 flex flex-col justify-between items-center"
+      className="absolute h-screen top-0 left-0 z-50 w-screen flex flex-col items-center justify-around"
     >
-      <div
-        onClick={() => setScreenshotViewer(false)}
-        className="self-end border border-white rounded-full w-7 h-7 lg:w-10 lg:h-10 flex items-center justify-center mt-5 mr-10 lg:mr-32 cursor-pointer"
-      >
-        <i className="fa-solid fa-xmark lg:text-3xl"></i>
+      <div className="flex flex-col items-center md:w-[70%] justify-center gap-y-5 max-h-[80%]">
+        <div
+          onClick={() => setScreenshotViewer(false)}
+          className="self-end border border-white rounded-full w-5 h-5 lg:w-8 lg:h-8 flex items-center justify-center cursor-pointer"
+        >
+          <i className="fa-solid fa-xmark lg:text-2xl"></i>
+        </div>
+        <figure className="flex items-center justify-center h-fit max-h-[90%] rounded-sm md:rounded overflow-hidden">
+          <img
+            className="w-full h-full object-contain"
+            src={picUrl}
+            alt="project-screenshot"
+          />
+        </figure>
       </div>
-      <figure className="flex items-center justify-center h-[70%] min-w-fit max-w-[90%] my-5 rounded-sm md:rounded-lg overflow-x-auto overflow-y-hidden">
-        <img
-          className="w-full h-full object-contain"
-          src={picUrl}
-          alt="project-screenshot"
-        />
-      </figure>
-      <ul className="flex gap-x-5 justify-center w-[90%]">
+      <ul className="flex gap-x-2 lg:gap-x-5 justify-start max-w-[90%] max-h-[85px] overflow-x-auto">
         {screenshots.map((s) => (
           <li key={s.id}>
-            <figure className="w-16 h-16 md:w-20 md:h-20 overflow-hidden rounded-sm md:rounded-lg flex items-center justify-center hover-pic target:border-2 target:border-teal-400">
+            <figure className="w-16 h-16 lg:w-20 lg:h-20 overflow-hidden rounded-sm md:rounded-lg flex items-center justify-center hover-pic target:border-2 target:border-teal-400">
               <img
                 onClick={(e) => setPicUrl(e.currentTarget.src)}
                 src={s.url}
