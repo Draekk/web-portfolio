208769c7710b3239f7d74dcda47975f6c069793f
Draekk <rodriver1992@gmail.com>
2025-05-13 19:51:10 -0400

    fixed error on tech viewer

--- client/src/components/technologies/Technologies.tsx
+++ client/src/components/technologies/Technologies.tsx
@@ -48,8 +48,6 @@ function Technologies({ title, btnScreenshot }: tTechnologiesProps) {
         </div>
         {selectTech.enabled ? (
           <TechViewer
-            selectTech={selectTech}
-            toggleTech={toggleTech}
             tech={tech}
             text={btnScreenshot.text}
             closeText={btnScreenshot.closeText}
